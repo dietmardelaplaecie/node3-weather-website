5b9acc5da76762231036a239f70c0e9f1bc4a342
Dietmar Delaplaecie <dietmar.delaplaecie>
2022-12-07 08:50:19 +0100

    new test

--- public/JS/app.js
+++ public/JS/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
- 
+
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     fetch('http://localhost:3000/weather?address='+location).then((response) => {
